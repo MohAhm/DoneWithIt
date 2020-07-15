29e7fd76f07909074f986e6ea3156e980f7d6765
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-15 14:11:45 +0200

    try and test fundamental concepts

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	SafeAreaView,
+	Image,
+	Alert,
+	Button,
+	Platform
+} from 'react-native'
+
 
 export default function App() {
+	const handlePress = () => console.log("It pressed")
+
 	return (
-		<View style={styles.container}>
-			<Text>Hello World!</Text>
+		<SafeAreaView style={styles.container}>
+			<Text onPress={handlePress}>Hello World!</Text>
+			<TouchableOpacity onPress={handlePress}>
+				<Image source={{
+					width: 200,
+					height: 300,
+					uri: "https://picsum.photos/200/300"
+				}} />
+			</TouchableOpacity>
+
+			{/* <Image source={require("./assets/icon.png")} /> */}
+
+			<Button
+				title="Click Me"
+				onPress={() => Alert.alert("My Title", "My Message", [
+					{text: "Yes", onPress: () => console.log("Yes")},
+					{text: "No", onPress: () => console.log("No")}
+				])} />
 			<StatusBar style="auto" />
-		</View>
+		</SafeAreaView>
 	);
 }
 
@@ -15,7 +43,8 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: "center",
+		alignItems: "center",
+		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
 	},
 });
