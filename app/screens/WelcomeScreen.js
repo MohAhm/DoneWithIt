7e27ff20e8c9928ae20db5f22f2a1495473d2ff5
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-18 12:42:50 +0200

    feat: create button component

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native'
+import AppButton from '../components/AppButton'
 import colors from '../config/colors'
 
 
 export default function WelcomeScreen() {
     return (
         <ImageBackground
+            blurRadius={10}
             style={styles.background}
             source={require("../assets/background.jpg")}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
 
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonContainer}>
+                <AppButton title={"Login"} />
+                <AppButton title={"Register"} color="secondary" />
+            </View>
         </ImageBackground>
     )
 }
@@ -33,21 +37,20 @@ const styles = StyleSheet.create({
         height: 100,
     },
 
+    tagline: {
+        fontSize: 25,
+        fontWeight: '600',
+        paddingVertical: 20,
+    },
+
     logoContainer: {
         alignItems: 'center',
         position: 'absolute',
         top: 70,
     },
 
-    loginButton: {
+    buttonContainer: {
+        padding: 20,
         width: '100%',
-        height: 70,
-        backgroundColor: colors.primary,
     },
-
-    registerButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: colors.secondary,
-    }
 })
