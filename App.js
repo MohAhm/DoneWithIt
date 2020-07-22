212aa47ed7cc76a62d0cda2549557b8fa425912e
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-22 11:50:41 +0200

    feat: add Icon component

--- App.js
+++ App.js
@@ -6,13 +6,23 @@ import ViewImageScreen from './app/screens/ViewImageScreen'
 import MessagesScreen from './app/screens/MessagesScreen'
 import Card from './app/components/Card'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+import Screen from './app/components/Screen'
+import Icon from './app/components/Icon'
 
 export default function App() {
 	return (
 		// <WelcomeScreen />
 		// <ViewImageScreen />
 		// <ListingDetailsScreen />
-		<MessagesScreen />
+		// <MessagesScreen />
+		<Screen>
+			<Icon
+				name='email'
+				size={50}
+				backgroundColor='red'
+				iconColor='white'
+			/>
+		</Screen>
 	)
 }
 
