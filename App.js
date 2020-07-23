8445f40aac4b055bf5c9034b962da5c53940b852
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-23 10:46:56 +0200

    feat: create TextInput component

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import MessagesScreen from './app/screens/MessagesScreen'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
 import AccountScreen from './app/screens/AccountScreen'
 import ListingsScreen from './app/screens/ListingsScreen'
+import Screen from './app/components/Screen'
+import AppTextInput from './app/components/AppTextInput'
 
 export default function App() {
 	return (
@@ -15,7 +17,11 @@ export default function App() {
 		// <ListingDetailsScreen />
 		// <MessagesScreen />
 		// <AccountScreen />
-		<ListingsScreen />
+		// <ListingsScreen />
+
+		<Screen>
+			<AppTextInput placeholder='Username' icon='email' />
+		</Screen>
 	)
 }
 
