a9dff7727739fc65fb782612c775b9a29112b7ae
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-23 11:31:22 +0200

    feat: add Picker component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
 import WelcomeScreen from './app/screens/WelcomeScreen'
 import ViewImageScreen from './app/screens/ViewImageScreen'
@@ -9,8 +9,17 @@ import AccountScreen from './app/screens/AccountScreen'
 import ListingsScreen from './app/screens/ListingsScreen'
 import Screen from './app/components/Screen'
 import AppTextInput from './app/components/AppTextInput'
+import AppPicker from './app/components/AppPicker'
+
+const categories = [
+	{ label: 'Furniture', value: 1 },
+	{ label: 'Clothing', value: 2 },
+	{ label: 'Cameras', value: 3 },
+]
 
 export default function App() {
+	const [category, setCategory] = useState(categories[0])
+
 	return (
 		// <WelcomeScreen />
 		// <ViewImageScreen />
@@ -20,7 +29,15 @@ export default function App() {
 		// <ListingsScreen />
 
 		<Screen>
-			<AppTextInput placeholder='Username' icon='email' />
+			<AppPicker
+				selectedItem={category}
+				onSelectedItem={item => setCategory(item)}
+				items={categories}
+				placeholder='Category'
+				icon='apps'
+			/>
+
+			<AppTextInput placeholder='Email' icon='email' />
 		</Screen>
 	)
 }
