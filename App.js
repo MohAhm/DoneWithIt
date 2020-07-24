007629a3e708a31f525babecfe14811e7b00e6bf
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-24 10:36:35 +0200

    feat: create login form

--- App.js
+++ App.js
@@ -1,44 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from 'react'
 import { View } from 'react-native'
-import WelcomeScreen from './app/screens/WelcomeScreen'
-import ViewImageScreen from './app/screens/ViewImageScreen'
-import MessagesScreen from './app/screens/MessagesScreen'
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
-import AccountScreen from './app/screens/AccountScreen'
-import ListingsScreen from './app/screens/ListingsScreen'
-import Screen from './app/components/Screen'
-import AppTextInput from './app/components/AppTextInput'
-import AppPicker from './app/components/AppPicker'
+import LoginScreen from './app/screens/LoginScreen'
 
-const categories = [
-	{ label: 'Furniture', value: 1 },
-	{ label: 'Clothing', value: 2 },
-	{ label: 'Cameras', value: 3 },
-]
 
 export default function App() {
-	const [category, setCategory] = useState(categories[0])
 
 	return (
-		// <WelcomeScreen />
-		// <ViewImageScreen />
-		// <ListingDetailsScreen />
-		// <MessagesScreen />
-		// <AccountScreen />
-		// <ListingsScreen />
-
-		<Screen>
-			<AppPicker
-				selectedItem={category}
-				onSelectedItem={item => setCategory(item)}
-				items={categories}
-				placeholder='Category'
-				icon='apps'
-			/>
-
-			<AppTextInput placeholder='Email' icon='email' />
-		</Screen>
+		<LoginScreen />
 	)
 }
 
