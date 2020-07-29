9ff1362fa8e2f320d750b553f72c74d0b5558f4b
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-07-29 12:56:42 +0200

    feat: use Tab Navigator

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import MessagesScreen from './app/screens/MessagesScreen'
 import ListingEditScreen from './app/screens/ListingEditScreen'
 import Screen from './app/components/Screen'
 
-import { createStackNavigator } from '@react-navigation/stack'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
 import { NavigationContainer, useNavigation } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 
 const Link = () => {
@@ -33,7 +36,7 @@ const TweetDetails = ({ route }) => (
 )
 
 const Stack = createStackNavigator()
-const StackNavigator = () => (
+const FeedNavigator = () => (
 	<Stack.Navigator
 		screenOptions={{
 			headerStyle: { backgroundColor: 'dodgerblue'},
@@ -49,10 +52,35 @@ const StackNavigator = () => (
 	</Stack.Navigator>
 )
 
+const Account = () => <Screen><Text>Account</Text></Screen>
+
+const Tab = createBottomTabNavigator()
+const TabNavigator = () => (
+	<Tab.Navigator
+		tabBarOptions={{
+			activeBackgroundColor: 'tomato',
+			activeTintColor: 'white',
+			inactiveBackgroundColor: '#eee',
+			inactiveTintColor: 'black',
+		}}
+	>
+		<Tab.Screen
+			name='Feed'
+			component={FeedNavigator}
+			options={{
+				tabBarIcon: ({ size, color }) => (
+					<MaterialCommunityIcons name='home' size={size} color={color} />
+				)
+			}}
+		/>
+		<Tab.Screen name='Account' component={Account}/>
+	</Tab.Navigator>
+)
+
 export default function App() {
 	return (
 		<NavigationContainer>
-			<StackNavigator />
+			<TabNavigator />
 		</NavigationContainer>
 	)
 }
