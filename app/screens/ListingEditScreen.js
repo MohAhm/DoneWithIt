ed392d45f98c1fb66762e40d3fb7377ba98d5bbd
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-09-05 11:11:52 +0200

    feat: saving the listing

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import * as Yup from 'yup'
 
@@ -11,7 +11,9 @@ import {
 import Screen from '../components/Screen'
 import CategoryPickerItem from '../components/CategoryPickerItem'
 import FormImagePicker from '../components/forms/FormImagePicker'
+import listingsApi from '../api/listings'
 import useLocation from '../hooks/useLocation'
+import UploadScreen from './UploadScreen'
 
 
 const validationSchema =  Yup.object().shape({
@@ -36,9 +38,33 @@ const categories = [
 
 export default function RegisterScreen() {
     const location = useLocation()
+    const [uploadVisible, setUploadVisible] = useState(false)
+    const [progress, setProgress] = useState(0)
+
+    const handleSubmit = async (listing, { resetForm }) => {
+        setProgress(0)
+        setUploadVisible(true)
+        const result = await listingsApi.addListing(
+            { ...listing, location },
+            progress => setProgress(progress)
+        )
+
+        if (!result.ok) {
+            setUploadVisible(false)
+            return alert('Could not save the listing.')
+        }
+
+        resetForm()
+    }
 
     return (
         <Screen style={styles.container}>
+            <UploadScreen
+                onDone={() => setUploadVisible(false)}
+                progress={progress}
+                visible={uploadVisible}
+            />
+
             <Form
                 initialValues={{
                     title: '',
@@ -47,7 +73,7 @@ export default function RegisterScreen() {
                     category: null,
                     images: [],
                 }}
-                onSubmit={values => console.log((location))}
+                onSubmit={handleSubmit}
                 validationSchema={validationSchema}
             >
                 <FormImagePicker name='images'/>
